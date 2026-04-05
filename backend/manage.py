624f677abdae4fbea2409ab354b0c062c3ddcb45
Craveto <snehaldighore89@gmail.com>
2026-04-05 15:33:00 +0530

    updated requirements.txt

--- backend/manage.py
+++ backend/manage.py
@@ -4,7 +4,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.config.settings")
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
@@ -12,3 +12,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
